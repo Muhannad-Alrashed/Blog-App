76ad5d1f328717bdfd57947496b0580005b71178
Muhannad <muhannad.1design@gmail.com>
2025-04-03 14:11:31 +0300

    Initial commit

---
+++ client/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAvSrLo88VFjeQgIZb4AzyhKPaVU_SJJ2Q",
+  authDomain: "blog-app-713f0.firebaseapp.com",
+  projectId: "blog-app-713f0",
+  storageBucket: "blog-app-713f0.firebasestorage.app",
+  messagingSenderId: "301427033122",
+  appId: "1:301427033122:web:4e80df82503b35b83c767b",
+  measurementId: "G-7T5DTNPK9H"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
